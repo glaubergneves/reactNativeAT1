95a05136b771e0d3ba9a8e5f1397291cb2018f20
Glauber Neves <glaubergneves@gmail.com>
2024-04-04 07:36:50 -0300

    implementa imagens ao bottom drawer

--- src/routes/ProductDetailsTabs.js
+++ src/routes/ProductDetailsTabs.js
@@ -5,13 +5,37 @@ import Comments from '../components/Comments';
 import Questions from '../components/Questions';
 import TechnicalSpecifications from '../components/TechnicalSpecifications';
 
+import { Image } from 'react-native';
+import specificationsIcon from '../../assets/specificationsIcon.png';
+import commentsIcon from '../../assets/commentsIcon.png';
+import questionsIcon from '../../assets/questionsIcon.png';
+import vendorInfoIcon from '../../assets/vendorInfoIcon.png';
+
 const Tab = createBottomTabNavigator();
 
 const ProductDetailsTabs = ({ route }) => {
   const { product } = route.params;
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconSource;
+
+          if (route.name === 'Especificações') {
+            iconSource = focused ? specificationsIcon : specificationsIcon;
+          } else if (route.name === 'Comentários') {
+            iconSource = focused ? commentsIcon : commentsIcon;
+          } else if (route.name === 'Perguntas') {
+            iconSource = focused ? questionsIcon : questionsIcon;
+          } else if (route.name === 'Vendedor') {
+            iconSource = focused ? vendorInfoIcon : vendorInfoIcon;
+          }
+
+          return <Image source={iconSource} style={{ width: 25, height: 25 }} />;
+        },
+      })}
+    >
       <Tab.Screen name="Especificações">
         {() => <TechnicalSpecifications specifications={product.technicalSpecifications} />}
       </Tab.Screen>
@@ -21,7 +45,7 @@ const ProductDetailsTabs = ({ route }) => {
       <Tab.Screen name="Perguntas">
         {() => <Questions questions={product.questions} />}
       </Tab.Screen>
-      <Tab.Screen name="Informações do Vendedor">
+      <Tab.Screen name="Vendedor">
         {() => <VendorInfo vendor={product.vendorData} />}
       </Tab.Screen>
     </Tab.Navigator>
